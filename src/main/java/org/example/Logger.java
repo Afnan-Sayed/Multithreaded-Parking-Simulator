04af857d1a0cb1c0e61d23e5c4b95dc342191c51
Ahmed Mohamed <114872733+GEMIv1@users.noreply.github.com>
2024-11-11 20:40:59 +0200

    Delete src directory

--- src/main/java/org/example/Logger.java
+++
@@ -1,8 +0,0 @@
-package main.java.org.example;//logger class -> logs each car from its arrival to leaving
-//-logArrival(Car car)
-//-logLeaving(Car car)
-//-logParkingStatus(parkingLot pl)-> bt-log el status b3d kol service
-
-public class Logger {
-
-}
